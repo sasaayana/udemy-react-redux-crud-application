c519a9bb99cca175dafed6104e9fdbe7013ff886
admin <admin@adminnoMacBook-Pro.local>
2020-07-01 17:05:59 +0900

    learn jsx syntax

--- react-pj/src/App.js
+++ react-pj/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 function App() {
+  //const greeting = "Hi, Tom! ";
+  //const dom = <h1 className="foo">{greeting}</h1>;
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am clicked")}} />
+    </div>
   );
 }
 
