4aaadf75d5a7668949a6aafa424c1f16143a8169
admin <admin@adminnoMacBook-Pro.local>
2020-07-01 20:20:30 +0900

    learn props of component

--- react-pj/src/App.js
+++ react-pj/src/App.js
@@ -23,20 +23,30 @@ import React from 'react';
 // }
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "NoName" }
+  ]
   return(
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
+const User = (props) => {
   return(
-    <div>Meow!</div>
+    <div>Hi, I am {props.name}, and {props.age} years old!</div>
   )
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
